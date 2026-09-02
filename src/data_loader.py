9ee5d377d6c6d70c46aa68f5b8f09d0b706d3656
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-09-02 23:39:58 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 import numpy as np
 def load_data():
-    import numpy as np
-def load_data():
     url='https://dagshub.com/harikrishnakumar368/EMIPredict-AI---Intelligent-Financial-Risk-Assessment-Platform-AIML-intern-labmentix/raw/main/s3:/EMIPredict-AI---Intelligent-Financial-Risk-Assessment-Platform-AIML-intern-labmentix/emi_prediction_dataset.csv'
     df = pd.read_csv(url)
-    return df   
+    print(df)
+    #df = df.to_pandas()
+    return df 
